e8f1e27ecb682e26ebf4e8fb1bdfcb6fe755606f
Wentz Chen <me@pannnda.com>
2020-01-17 01:49:29 +0800

    feat: add websites

--- index.js
+++ index.js
@@ -1,51 +1,62 @@
-require('dotenv').config()
-
-const axios = require('axios')
-const moment = require('moment')
-const { Toolkit } = require('actions-toolkit')
-const { GistBox } = require('gist-box')
+require("dotenv").config();
+const axios = require("axios");
+const moment = require("moment");
+const { Toolkit } = require("actions-toolkit");
+const { GistBox } = require("gist-box");
 
 Toolkit.run(
   async tools => {
-    const { GIST_ID, GH_USERNAME, GH_PAT } = process.env
+    const { GIST_ID, GH_USERNAME, GH_PAT } = process.env;
 
     // Get the user's public events
-    tools.log.debug(`Getting activity for ${GH_USERNAME}`)
+    tools.log.debug(`Getting activity for ${GH_USERNAME}`);
 
-    let closed = true
-    for (let i = 0; i < 3; i++) {
-      await axios.get('https://www.zhihu.com').then(res => {
-        if (res.status === 200) {
-          closed = false
-        }
-      })
+    let websites = [
+      {
+        name: "pannnda",
+        url: "https://www.pannnda.com"
+      },
+      {
+        name: "blog",
+        url: "https://blog.pannnda.com"
+      },
+      {
+        name: "jwzx",
+        url: "https://jwzx.pannnda.com"
+      }
+    ];
+    let status = [];
+
+    for (let i = 0; i < websites.length; i++) {
+      let closed = true;
+      let res = await axios.get(websites[i].url);
+      if (res.status === 200) {
+        closed = false;
+      }
+      status.push({
+        name: websites[i].name,
+        closed
+      });
     }
 
-    const time = moment().format('YYYY-MM-DD kk:mm ZZ')
+    const time = moment().format("YYYY-MM-DD kk:mm ZZ");
 
-    let content = ''
-    if (closed) {
-      content = `down
-${time}
-      `
-    } else {
-      content = `up
-${time}
-      `
-    }
+    let content = status
+      .map(s => `${s.name}: ${s.closed ? "closed." : "running..."}\n`)
+      .join("");
 
-    const box = new GistBox({ id: GIST_ID, token: GH_PAT })
+    const box = new GistBox({ id: GIST_ID, token: GH_PAT });
     try {
-      tools.log.debug(`Updating Gist ${GIST_ID}`)
-      await box.update({ content })
-      tools.exit.success('Gist updated!')
+      tools.log.debug(`Updating Gist ${GIST_ID}`);
+      await box.update({ content });
+      tools.exit.success("Gist updated!");
     } catch (err) {
-      tools.log.debug('Error getting or update the Gist:')
-      return tools.exit.failure(err)
+      tools.log.debug("Error getting or update the Gist:");
+      return tools.exit.failure(err);
     }
   },
   {
-    event: ['schedule', 'push'],
-    secrets: ['GH_PAT', 'GH_USERNAME', 'GIST_ID']
+    event: ["schedule", "push"],
+    secrets: ["GH_PAT", "GH_USERNAME", "GIST_ID"]
   }
-)
+);
